d6a47f829e9fe70e038e4660c9821728b432a454
osemak <osemak.dataart@gmail.com>
2022-02-01 15:08:01 +0000

    add jsdoc comments

--- frontend/src/utils/getTravelDataByFieldName/getTravelDataByFieldName.ts
+++ frontend/src/utils/getTravelDataByFieldName/getTravelDataByFieldName.ts
@@ -1,20 +1,39 @@
 import { ITravel } from "../../interfaces/travels.interfaces";
 
-const getUniqueValues = (valuesArray: string[]) =>
+/**
+ * Get unique values
+ * @function
+ * @param valuesArray 
+ * @returns {Array.<string>}
+ */
+const getUniqueValues = (valuesArray: string[]): string[] =>
     valuesArray.filter(
         (property: string, index: number, array: string[]) =>
             array.indexOf(property) === index
     );
 
-const sortArrayAlphabetically = (valuesArray: string[]) =>
+/**
+ * Sort array of values alphabetically
+ * @function
+ * @param valuesArray 
+ * @returns {Array.<string>}
+ */
+const sortArrayAlphabetically = (valuesArray: string[]): string[] =>
     [...valuesArray].sort((currentString, nextString) =>
         currentString.localeCompare(nextString)
     );
 
+/**
+ * Get travel data by field name to fill Redux store
+ * @function
+ * @param {array} travelData - travel data from DB
+ * @param {string} propertyName - field name which exists in travel data instance
+ * @returns {Array.<string>}
+ */
 export const getTravelDataByFieldName = (
     travelData: ITravel[],
     propertyName: string
-) => {
+): string[] => {
     const valuesByPropertyName = travelData.map((location) =>
         location[propertyName] ? location[propertyName] : "Not provided"
     );
